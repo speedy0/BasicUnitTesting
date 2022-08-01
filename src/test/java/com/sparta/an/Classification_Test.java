15b6b6c7b89d6ee35e48bf3dd2e904a373f6debb
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-01 18:53:43 +0100

    Adding Testing Cases

--- src/test/java/com/sparta/an/Classification_Test.java
+++ src/test/java/com/sparta/an/Classification_Test.java
@@ -14,24 +14,30 @@ public class Classification_Test {
     @Test
     @DisplayName("If Age is less than 8")
     void ifAgeIsLessThan8(){
-        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(7));
+        Assertions.assertEquals("U films are available.", FilmClassifications.availableClassifications(7));
     }
 
     @Test
     @DisplayName("If Age is less than 12")
     void ifAgeIsLessThan12(){
-        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(11));
+        Assertions.assertEquals("U & PG films are available.", FilmClassifications.availableClassifications(11));
     }
 
     @Test
     @DisplayName("If Age is less than 15")
     void ifAgeIsLessThan15(){
-        Assertions.assertEquals("U, PG, 12 & 15 films are available.", FilmClassifications.availableClassifications(14));
+        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(14));
     }
 
     @Test
-    @DisplayName("If Age is 15")
-    void ifAgeIs15(){
-        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(15));
+    @DisplayName("If Age is 15 but less than 18")
+    void ifAgeIs15ButLessThan18(){
+        Assertions.assertEquals("U, PG, 12 & 15 films are available.", FilmClassifications.availableClassifications(15));
+    }
+
+    @Test
+    @DisplayName("If Age is 18 or more")
+    void ifAgeIs18OrMore18(){
+        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(18));
     }
 }
