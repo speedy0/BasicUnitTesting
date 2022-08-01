0535270dc17346416394242dbab4b20f63271420
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-01 15:39:46 +0100

    Changes using web

--- src/main/java/com/sparta/an/FilmClassifications.java
+++ src/main/java/com/sparta/an/FilmClassifications.java
@@ -3,9 +3,15 @@ package com.sparta.an;
 public class FilmClassifications {
     static String availableClassifications(int ageOfViewer) {
         String result;
-        if (ageOfViewer < 12) {
+        if (ageOfViewer < 0){
+            result = "The age provided is not a valid.";
+        } else if (ageOfViewer < 8){
+            result = "U films are available.";
+        } else if (ageOfViewer < 12){
+            result = "U & PG films are available.";
+        }else if (ageOfViewer >= 12 && ageOfViewer < 15) {
             result = "U, PG & 12 films are available.";
-        } else if (ageOfViewer < 15) {
+        } else if (ageOfViewer >= 15 && ageOfViewer < 18) {
             result = "U, PG, 12 & 15 films are available.";
         } else {
             result = "All films are available.";
--- src/test/java/com/sparta/an/Classification_Test.java
+++ src/test/java/com/sparta/an/Classification_Test.java
@@ -6,6 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class Classification_Test {
     @Test
+    @DisplayName("If Age is less than 0")
+    void ifAgeIsLessThan0(){
+        Assertions.assertEquals("The age provided is not a valid.", FilmClassifications.availableClassifications(-1));
+    }
+
+    @Test
+    @DisplayName("If Age is less than 8")
+    void ifAgeIsLessThan8(){
+        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(7));
+    }
+
+    @Test
     @DisplayName("If Age is less than 12")
     void ifAgeIsLessThan12(){
         Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(11));
