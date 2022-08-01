8f7ed693c553f44813da6ec8fee62e5bcf602bfa
Aleksander Napieralski <aleksander.napieralski@city.ac.uk>
2022-08-01 15:06:18 +0100

    Creation of test cases for Classification Test

--- src/test/java/com/sparta/an/Classification_Test.java
+++ src/test/java/com/sparta/an/Classification_Test.java
@@ -1,7 +1,25 @@
 package com.sparta.an;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Classification_Test {
     @Test
+    @DisplayName("If Age is less than 12")
+    void ifAgeIsLessThan12(){
+        Assertions.assertEquals("U, PG & 12 films are available.", FilmClassifications.availableClassifications(11));
+    }
+
+    @Test
+    @DisplayName("If Age is less than 15")
+    void ifAgeIsLessThan15(){
+        Assertions.assertEquals("U, PG, 12 & 15 films are available.", FilmClassifications.availableClassifications(14));
+    }
+
+    @Test
+    @DisplayName("If Age is 15")
+    void ifAgeIs15(){
+        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(15));
+    }
 }
